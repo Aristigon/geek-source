b2568e1ac9ae18fe4513efa1623e0765e2c80f93
jamesmdavisjr <jamesmdavisjr.se@gmail.com>
2021-05-20 08:39:38 -0400

    testing 2

--- jd-geek-source/src/app/app.component.ts
+++ jd-geek-source/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(navigator.userAgent);
+    console.log(navigator.platform);
 
     if (window.innerWidth <= MOBILE_SCREEN_THRESHOLD) {
       this.mobileSite = true;
